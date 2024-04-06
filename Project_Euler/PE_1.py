8f813fa5c6f59b675c2033f8a5c315db7a37f085
Vaishnavi Kesiraju <vaishnavi.kesiraju@gmail.com>
2024-04-06 20:13:43 +0530

    Approach 1 PE_1.py

---
+++ Project_Euler/PE_1.py
@@ -0,0 +1,18 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+<p>Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+def check_divisible_3_5(num: int) -> bool:
+    if num % 3 == 0 or num % 5 == 0:
+        return True
+    return False
+
+def divisible_sum(num: int) -> int:
+    sum1 = 0
+    for i in range(num):
+        if check_divisible_3_5(i) == True:
+            sum1 += i
+    return sum1
+
+print(divisible_sum(1000))
