62b0e7c493d4c6fd12bb47d53b743fc39d8b3118
Vaishnavi Kesiraju <vaishnavi.kesiraju@gmail.com>
2024-04-06 22:18:12 +0530

    Approach 1 LC_1.py

---
+++ LeetCode/LC_1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for num1 in range(len(nums)):
+            for num2 in range(num1 + 1, len(nums)):
+                if nums[num1] + nums[num2] == target:
+                    return [num1, num2]
