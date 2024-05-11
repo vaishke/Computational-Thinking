a125db3b8266a19bc0b5e2077884c7ea850b8068
Vaishnavi K <vaishnavi.kesiraju@gmail.com>
2024-05-11 19:55:31 +0530

    Update Collatz_Seq.py

--- WE_Classes/Collatz_Seq.py
+++ WE_Classes/Collatz_Seq.py
@@ -1,1 +1,10 @@
+# Program for generating Collatz Sequence Numbers
 
+def next_collatz(num: int) -> int:
+    return num // 2 if num % 2 == 0 else 3 * num + 1
+
+def gen_collatz(num: int) -> list[int]:
+    if num == 4:
+        return [4, 2, 1]
+    else:
+        return [num] + gen_collatz(next_collatz(num))
