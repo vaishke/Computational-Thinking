bc76838e3885fc943afcd7b5caea19da3a261a54
Vaishnavi K <vaishnavi.kesiraju@gmail.com>
2024-05-13 11:36:42 +0530

    Update grouping_anagrams.py

--- WE_Classes/grouping_anagrams.py
+++ WE_Classes/grouping_anagrams.py
@@ -1,1 +1,14 @@
+# Grouping Anagrams from a list of given words
 
+def key_maker(s: str) -> str:
+    return ''.join(sorted(s))
+
+def collect_anagrams(words: list[str]) -> list[set[str]]:
+    candidates = dict()
+    for word in words:
+        key = key_maker(word)
+        if key not in candidates:
+            candidates[key] = {word}
+        else:
+            candidates[key].add(word)
+    return [anags for anags in candidates.values() if len(anags) > 1]
